e1c7dec19355ffb5251250a34c1a1b9c5958be60
Merlina <noahchukita@gmail.com>
2024-08-20 20:41:26 -0300

    feat: agrega loader página principal

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,34 @@
-const createMovie = require('./createMovie')
-const renderCards = require('./renderCards')
-const axios = require('axios')
+import createMovie from './createMovie';
+import renderCards from './renderCards';
+import axios from 'axios';
+import 'swiper/swiper-bundle.css';
+import 'swiper/css/bundle';
 
 const fetchingData = async() => {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+    
     try {
-        const data = await axios.get(`${process.env.VITE_BACK_URL}/movies`)
-        renderCards(data.data)
+        const response = await axios.get(`${process.env.VITE_BACK_URL}/movies`);
+
+        if (response.data && response.data.length > 0) {
+            setTimeout(() => {
+                renderCards(response.data);
+                loader.style.display = 'none'; // Ocultar el loader
+                content.classList.remove('hidden'); // Mostrar el contenido
+            }, 2000)
+        } else {
+            loader.textContent = "No hay datos disponibles.";
+        }
+
     } catch (error) {
-       console.log(error)
-       console.log(process.env.VITE_BACK_URL)
-       throw new Error('La petición no tuvo exito')
+       console.error('La petición no tuvo éxito', error);
+       loader.textContent = "Error al cargar los datos.";
     }
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    fetchingData()
-    const submit = document.getElementById("form")
-    submit?.addEventListener('submit', createMovie)
-})
-
-
-
-
-
+    fetchingData();
+    const submit = document.getElementById("form");
+    submit?.addEventListener('submit', createMovie);
+});
